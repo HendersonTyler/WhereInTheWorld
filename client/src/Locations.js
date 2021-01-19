2a7c169cf748671630fdabd8f5e500ee15639b60
HendersonTyler <henderson.tyler@outlook.com>
2021-01-19 12:41:59 +1000

    Finished play again page

--- client/src/Locations.js
+++ client/src/Locations.js
@@ -13,14 +13,3 @@ export const locations = [
 ]
 
 export default locations;
-
-// export const locations = [
-//     ['Hong Kong', {lat: 22.31, lng: 114.17}], // add 2 digits
-//     ['New York', {lat: 40.730610, lng: -74.0060}], 
-//     ['London', {lat: 51.514550, lng: -0.137011}],
-//     ['Sydney', {lat: -33.874021, lng: 151.219131}],
-//     ['Bangkok', {lat: 13.747186947962735, lng: 100.49924671067122}],
-//     ['Berlin', {lat: 52.51928412408187, lng: 13.3944805613668}],
-// ]
-
-// export default locations;
